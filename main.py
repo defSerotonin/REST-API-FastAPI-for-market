12087066beac5fab3c2a1c7f2295402b07d71553
Serotonin <83265134+defSerotonin@users.noreply.github.com>
2024-07-06 23:52:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,68 +7,70 @@ import datetime
 
 app = FastAPI()
 
+json_folder = "Json_f"
 
+class UserRegister(BaseModel):
+    user_id: int
+    user_name: str
+    user_surname: str
+    user_city: str
+    user_mail: str
+    user_date: datetime.datetime
 
-activities_folder = "activities"
+class UserBlog(BaseModel):
+    user_message: str
+    user_message_datetime: datetime.datetime
 
+def find_json(json_user_id=0):
+    return glob.glob(os.path.join(json_folder, f'*{json_user_id if json_user_id else ""}.json'))
 
-class Activity(BaseModel):
-    id: int
-    name: str
-    date: datetime.datetime
-
-def find_activity(activity_id=0):
-    return glob.glob(os.path.join(activities_folder, f'*{activity_id if activity_id else ""}.json'))
-
-
-@app.get('/activity/get/all')
-def get_activity():
+@app.get('/users/get/all')
+def get_users_list():
     res = []
-    files = find_activity()
+    files = find_json()
     if files:
         for file in files:
-            res.append(parse_file_as(Activity, file))
+            res.append(parse_file_as(UserRegister, file))
     return res
 
-
-@app.get('/activity/add/{activity_id}')
-def get_activity(activity_id:int, response:Response):
-    files = find_activity(activity_id)
+@app.get('/users/add/{json_user_id}')
+def get_users_json(json_user_id:int, response:Response):
+    files = find_json(json_user_id)
     if files:
-        return parse_file_as(Activity, files[0])
+        return parse_file_as(UserRegister, files[0])
     response.status_code = status.HTTP_404_NOT_FOUND
     return {}
 
-
-
-@app.post('/activity/add/{activity_id}')
-def add_activity(activity_id:int, activity:Activity, response:Response):
-    file = find_activity(activity_id)
+@app.post('/users/post/{json_user_id}')
+def add_user(json_user_id:int, user:UserRegister, response:Response):
+    file = find_json(json_user_id)
     if file:
         response.status_code = status.HTTP_302_FOUND
         return False
     else:
-        with open(os.path.join(activities_folder, f'activity_{activity_id}.json'), 'w') as f:
-            f.write(activity.json())
-    return activity_id
+        with open(os.path.join(json_folder, f'user_{json_user_id}.json'), 'w') as f:
+            f.write(user.json())
+    return json_user_id
 
-@app.put('/activity/update/{activity_id}')
-def update_activity(activity_id:int, activity:Activity, response:Response):
-    file = find_activity(activity_id)
+@app.put('/users/update/{json_user_id}')
+def update_user(json_user_id:int, user:UserRegister, response:Response):
+    file = find_json(json_user_id)
     if file:
-        act = parse_file_as(Activity, file[0])
-        act.name = activity.name
-        act.date = activity.date
-        with open(os.path.join(activities_folder, f'activity_{activity_id}.json'), 'w') as f:
-            f.write(activity.json())
+        act = parse_file_as(UserRegister, file[0])
+        act.user_name = user.user_name
+        act.user_surname = user.user_surname
+        act.user_city = user.user_city
+        act.user_mail = user.user_mail
+        act.user_date = user.user_date
+        with open(os.path.join(json_folder, f'user_{json_user_id}.json'), 'w') as f:
+            f.write(user.json())
         return True
     response.status_code = status.HTTP_404_NOT_FOUND
     return False
 
-
-@app.delete('/activity/delete/{activity_id}')
-def delete_activity(activity_id:int, response:Response):
-    file = find_activity(activity_id)
+@app.delete('/users/delete/{json_user_id}')
+def delete_user_data(json_user_id:int, response:Response):
+    file = find_json(json_user_id)
     if file:
         os.remove(file[0])
         return True
